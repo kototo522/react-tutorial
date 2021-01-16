5ba350eeb5b072bbbba075afbe57bf1e5ebab9a5
kototo522 <huyejiben@gmail.com>
2021-01-16 14:28:17 +0900

    #7 修正

--- src/List.js
+++ src/List.js
@@ -8,19 +8,22 @@ const Container = styled.div`
 const ListItem = styled.div`
     padding: 8px 16px;
 
-    &::nth-child(n+2){
-        border-top: 1px solid #D9DBdE;
+    &:nth-child(n+2) {
+        border-top: 1px solid #D9DBDE;
     }
 `
 
 export const List = ({ langs }) => {
     return(
         <Container>
+            <h4>取り扱い言語リスト</h4>
+            <div>
         {
             langs.map((lang, index) => {
                 return <ListItem key = {index}>{ lang }</ListItem>
             })
         }
+            </div>
         </Container>        
     )
 };
